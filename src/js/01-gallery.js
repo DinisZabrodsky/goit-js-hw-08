ffaf3ef6e7c9a13b51bea77c8eb60c94a5f7d101
Dinis PC <DinisZabrodsky@gmail.com>
2023-03-13 23:15:43 +0200

    002

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML("beforeend", createGalery(galleryItems));
+// gallery.addEventListener('click', openImage);
+
+
+function createGalery(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    }).join("");
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+const STOREG_KEY = "feedback-form-state";
+const dataInput = {};
+
+saveData();
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(valueData, 500));
+
+function valueData(event) {
+    dataInput.email = email.value;
+    dataInput.message = message.value;
+
+    console.log(dataInput);
+
+    localStorage.setItem(STOREG_KEY, JSON.stringify(dataInput));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+    console.log(dataInput);
+
+    event.currentTarget.reset();
+    localStorage.removeItem(STOREG_KEY);
+};
+
+function saveData() {
+    const arreyData = JSON.parse(localStorage.getItem(STOREG_KEY));
+
+    if (localStorage.getItem(STOREG_KEY)) {
+        email.value = arreyData.email;
+        message.value = arreyData.message;
+    };
+}
