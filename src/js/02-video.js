4286a312c6531054006ad3296937b01292509b26
Dinis PC <DinisZabrodsky@gmail.com>
2023-03-13 21:34:28 +0200

    002

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(function (data) {
+    console.log(data)
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data))
+}), 1000);
+
+goSeveSecond();
+
+function goSeveSecond() {
+    const seconArray = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+    if (seconArray) {
+        player.setCurrentTime(seconArray.seconds);
+    }
+}
